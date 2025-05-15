7e12a15f107227d0d195cbd8b80424eda745c5b3
fanomezana <hei.fanomezana.2@gmail.com>
2025-05-15 20:34:56 +0300

    feat: add a trigger for menu

--- src/main/java/com/hei/Main.java
+++ src/main/java/com/hei/Main.java
@@ -1,7 +1,16 @@
 package com.hei;
 
+import com.hei.model.UssdState;
+import com.hei.service.MenuService;
+import com.hei.service.TransactionService;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+
+    UssdState ussdState = new UssdState();
+    TransactionService service = new TransactionService();
+    MenuService menu = new MenuService(ussdState, service);
+
+    menu.start();
   }
 }
